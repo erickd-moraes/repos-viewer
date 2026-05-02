efe1923933dc1f3b05f57bb39bed4093b15061a7
erickd-moraes <erickd.moraes@gmail.com>
2026-05-02 12:16:56 -0300

    feat: implements base structure and toggle mode (light/dark)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export function App() {
   return (
@@ -8,7 +9,10 @@ export function App() {
           <h1 className="font-medium">Project ready!</h1>
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+          <div className="mt-2 flex gap-2">
+            <Button>Button</Button>
+            <ModeToggle />
+          </div>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import { ThemeProvider } from "@/components/theme-provider.tsx"
+
 import "./index.css"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
